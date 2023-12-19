eeb408612411df32df0989fcc163526f552a20dd
João Pedro <joaopdesousa12@gmail.com>
2023-12-19 08:08:59 -0300

    Nova resolucao 1708

--- python/1708/1708.py
+++ python/1708/1708.py
@@ -2,15 +2,9 @@ mais_rapido, mais_lento = map(int,input().split())
 
 voltas_mais_rapido = mais_rapido 
 voltas_mais_lento = mais_lento
-count = 0
+count = 1
 
-while(True):
-    if(voltas_mais_lento - voltas_mais_rapido >= mais_lento):
-        count += 1
-        
-        break
-    
-      
+while(voltas_mais_lento - voltas_mais_rapido < mais_lento):
     voltas_mais_rapido += mais_rapido
     voltas_mais_lento += mais_lento
     count += 1
