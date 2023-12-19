83c9a973501df6f764f10abb9a6e009f63bdd17f
João Pedro <joaopdesousa12@gmail.com>
2023-12-19 16:39:21 -0300

    Teste

---
+++ Java/1103_/1103.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int h1 = sc.nextInt();
+    int h2 = sc.nextInt();
+    int m1 = sc.nextInt();
+    int m2 = sc.nextInt();
+
+    int totalH = h2-h1;
+    int totalM = m2-m1;
+    
+
+    
+    if (h1 <  h2){
+      
+
+    }
+    
+
+
+
+    sc.close();
+  }
+}
--- Java/1171/1171.java
+++ Java/1171/1171.java
@@ -5,9 +5,9 @@ class Main{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int x,y;
     TreeMap<Integer, Integer> hMap = new TreeMap<>();
-    
+
+    int x, y;
     for (int i = 0; i < n; i++) {
       x = sc.nextInt();
 
